abe4c2ca61004758cd73b6a5a3a536044423e227
huangwanling <huangwanling154@gmail.com>
2025-12-01 15:29:44 +0800

    V1_黃婉凌

---
+++ app/src/main/java/tw/edu/pu/csim/tcyang/s1135208/ExamScreen.kt
@@ -0,0 +1,7 @@
+package tw.edu.pu.csim.tcyang.s1135208
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ExamScreen() {
+}
--- app/src/main/java/tw/edu/pu/csim/tcyang/s1135208/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/s1135208/MainActivity.kt
@@ -1,49 +1,45 @@
-package tw.edu.pu.csim.tcyang.s1135208
-
+import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import tw.edu.pu.csim.tcyang.s1135208.ExamScreen
 import tw.edu.pu.csim.tcyang.s1135208.ui.theme.S1135208Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // 強制直式
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
+        // 隱藏狀態列與導航列（全螢幕）
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        window.decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                )
+
         enableEdgeToEdge()
         setContent {
             S1135208Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ExamScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    S1135208Theme {
-        Greeting("Android")
     }
 }
